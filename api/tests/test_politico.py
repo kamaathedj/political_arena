157d7d73309f0786eea4d891c7d0e50253fe4e01
kamaathedj <kamaudavikiruku@gmail.com>
2019-02-08 16:51:13 +0300

    adding more tests

--- api/tests/test_politico.py
+++ api/tests/test_politico.py
@@ -9,6 +9,7 @@ class test_politico(unittest.TestCase):
         self.app=app
         self.client=self.app.test_client()
         self.data_party={"id":1,"name":"odm","hqAddress":"nairobi","logoUrl":"gfgfgf"}
+        self.office={"name":"uhuru kenyatta","type":"president"}
 
     def post(self, path='/parties', data={}):
         if not data:
@@ -17,6 +18,15 @@ class test_politico(unittest.TestCase):
         resp = self.client.post(path='api/v1/parties', data=json.dumps(self.data_party), content_type='application/json')
         
         return resp
+
+    def post_office(self, path='/offices', data={}):
+        if not data:
+            data = self.office
+
+        resp = self.client.post(path='api/v1/', data=json.dumps(self.data_party), content_type='application/json')
+        
+        return resp
+
 
     def test_posting_a_political_party(self):
         resp = self.post()
@@ -51,13 +61,20 @@ class test_politico(unittest.TestCase):
         resp=self.client.get(path='api/v1/getoffices',content_type='application/json')
         self.assertEqual(resp.status_code,200)
 
-    # def test_specific_office(self):
-    #     office = self.post()
-    #     path = '/api/v1/specificoffice/0'
-    #     response = self.client.get(path, content_type='application/json')
-    #     self.assertEqual(response.status_code, 200)
+    def test_specific_office(self):
+        office = self.post_office()
+        path = '/api/v1/specificoffice/1'
+        response = self.client.get(path, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
 
+    def test_delete_specific_office(self):
+        response=self.client.delete("api/v1/deleteparty/0",content_type='application/json')
+        # import pdb; pdb.set_trace()
+        self.assertNotIn(response.data,"office deleted succesfully")
+
+    
+        
     
         
 
