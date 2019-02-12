72839e46afdddb88892fe87609228b973e29ca72
kamaathedj <kamaudavikiruku@gmail.com>
2019-02-12 12:57:23 +0300

    [Finishes #163894951]final iteration of validation

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,6 +1,7 @@
+import ast
 from api.v1.models import createParty,CreateOffice,POLITICAL_OFFICE,PARTIES_DATA
 
-from flask import Flask,jsonify,request,logging,Blueprint,make_response
+from flask import Flask,jsonify,request,logging,Blueprint,make_response, json
 
 
 app=Flask(__name__)
@@ -15,29 +16,36 @@ office_data=CreateOffice()
 def create():
     data=request.get_json(force=True)
     # app.logger.info(data)
-    
-    id=len(PARTIES_DATA)+1
-    name=data['name']
-    hqAddress=data['hqAddress']
-    logoUrl=data['logoUrl']
-    # PARTY.append[data]
-#     new_party=createParty(id,name,hqAddress,logoUrl)
-
-    new_party={
-            "id":id,
-            "name":name,
-            "hqAddress":hqAddress,
-            "logoUrl":logoUrl
-    }
+    response=request.get_json(force=True)
+    data = json.dumps(response)
+    dictData = ast.literal_eval(data)
+    if dictData:
+            id=len(POLITICAL_OFFICE)+1
+            name=dictData["name"]
+            hqAddress=dictData["hqAddress"]
+            logoUrl=dictData["logoUrl"]
+            
+            if isinstance(name, str) and isinstance(hqAddress, str) and isinstance(logoUrl,str):
+                    new_party={ "id":id, "name":name,"hqAddress":hqAddress,"logoUrl":logoUrl}
    
-    party_data.parties(new_party)
+                    party_data.parties(new_party)
+                    message="Created" 
+                    return jsonify({"message":message},201) 
+                    
+            else:
+                    return jsonify({"status":400,"message":"all the data must be type string"},400)
+    else:
+            return jsonify({  "status":400, "message":"No data found" },400)
 
+                
+        
+             
+                 
+             
+                
+            
     
-    
-    return make_response(jsonify({
-            "status":201,
-            "msg":"Created"
-    }),201)
+   
 
 @userbp.route('/parties', methods=['GET'])
 def GetParties():
@@ -46,19 +54,27 @@ def GetParties():
         return jsonify({"message":parties})
     
 
-@userbp.route('/party/<party_id>',methods=['GET'])
+@userbp.route('/party/<party_id>', methods=['GET'])
 def GetSpecificParty(party_id):
-       
-        
-        specific_party=party_data.getPartyId(party_id)
-      
-        mparty=[]
-         
-        mparty.append(specific_party)
+        try:
+                id=int(party_id)
+        except:
+                return jsonify({"status" : 404, "message": "not found"}) 
 
+        specific_party=party_data.getPartyId(id)
+        if specific_party is None:
+                return jsonify({"status" : 404, "message": "not found"}) 
+        else:
+                mparty=[]
+
+                mparty.append(specific_party)
+
+                return jsonify({"status" : 200, "message": mparty})           
+
+
+
+      
         
-        return jsonify({"message": mparty})
-     
         
         
 
@@ -93,28 +109,41 @@ def deleteParty(partyid):
       
 @userbp.route('/offices', methods=['POST'])
 def createOffice():
-    message=""
-    data=request.get_json()
+
+    response=request.get_json(force=True)
+    data = json.dumps(response)
+    dictData = ast.literal_eval(data)
+    
+    print(data)
+    
+
     if data:
             id=len(POLITICAL_OFFICE)+1
-            name=data['name']
-            mtype=data['type']
-    
-    
-   
-            new_office={ "id":id, "name":name,"type":mtype}
-            office_data.offices(new_office)
-            message="Created"
-            
-            
+           
+            name=dictData["name"]
+            mtype=dictData["type"]
+            if isinstance(name, str) and isinstance(mtype, str):
+                    print(type(name),type(mtype))
+                    new_office={ "id":id, "name":name,"type":mtype}
+                    office_data.offices(new_office)
+                    message="Created" 
+                    return jsonify({"message":message},201) 
+                    
+            else:
+                    return jsonify({"status":400,"message":"all the data must be type string"},400)
+                
+        
+             
+                 
+                    
+                     
            
     else:
-            
-            message="no data"
+            return jsonify({  "status":400, "message":"No data found" },400)
     
     
 
-    return jsonify({"message":message},201)
+   
     
     
 @userbp.route('/offices', methods=['GET'])
@@ -126,31 +155,28 @@ def GetPoliticalOffices():
 
 
 
-@userbp.route('/offices/<int:office_id>',methods=['GET'])
+@userbp.route('/offices/<office_id>',methods=['GET'])
 def GetSpecificOffice(office_id):
-        specific_office=office_data.GetOfficeId(office_id)
-      
-        mOffice=[]
-         
-        mOffice.append(specific_office)
-
+        try:
+                id=int(office_id)
+              
+        except:
+                return jsonify({"status":404,"Message":"not found"})
+        variable_types = (int,)
+        print(variable_types,type(variable_types))
+        if isinstance(id,variable_types):
+                specific_office=office_data.GetOfficeId(id)
         
-        return jsonify({"message": mOffice})
+                mOffice=[]
+                
+                mOffice.append(specific_office)
+
+                return jsonify({"message": mOffice})
+        else:
+                return jsonify({"status":404,"Message":"id must be of type int"},404)
      
 
-        
-       
-
-
-
-            
-                        
-                        
-         
-
-       
-        
-
+#       
     
      
         
