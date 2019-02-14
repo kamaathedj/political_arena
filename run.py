651bb3321e5ae942d4bd7aaab6f6f2dc8aa96a56
kamaathedj <kamaudavikiruku@gmail.com>
2019-02-14 16:02:24 +0300

    fix travis

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from api import creating_app
 from api.v1.views import create
 import os
 
-app=creating_app(enviroment=os.getenv('FLASK_ENV'))
+app=creating_app(enviroment=os.getenv('FLASK_CONFIG'))
 
 if __name__=='__main__':
     app.run()
