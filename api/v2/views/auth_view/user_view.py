6e3be54bfe798cc437d629982c7882117428e8ae
kamaathedj <kamaudavikiruku@gmail.com>
2019-02-18 16:22:57 +0300

    [Finishes #163951487]user registration

--- api/v2/views/auth_view/user_view.py
+++ api/v2/views/auth_view/user_view.py
@@ -1,7 +1,15 @@
 from flask import jsonify,request,Blueprint
+from api.v2.models.authentication_model.userModel import user
+import ast
+import json
 
 v2_bp = Blueprint('apiv2', __name__, url_prefix='/api/v2')
 
 @v2_bp.route('/users', methods=['POST'])
 def createUser():
-    return jsonify({"message":"time to start v2"})
+    data=request.get_json(force=True)
+    jsodata=json.dumps(data)
+    datadict=ast.literal_eval(jsodata)
+    resp=user(datadict).createUser()
+
+    return jsonify({"message":"The account was created","message 2":resp})
