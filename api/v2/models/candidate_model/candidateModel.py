eb527adf1f02c9f29911983f93d7907c7a65b306
kamaathedj <kamaudavikiruku@gmail.com>
2019-03-11 09:39:16 +0300

    final iteration of candidate endpoints

--- api/v2/models/candidate_model/candidateModel.py
+++ api/v2/models/candidate_model/candidateModel.py
@@ -1,21 +1,30 @@
 from api.databaseConfig import connection
 
 class candidates:
-    def __init__(self,user):
-        self.user=user
+    def __init__(self,user_id,office_id):
+        self.user=user_id
+        self.office=office_id
 
     def registerCandidate(self):
         conn=connection()
         cur=conn.cursor()
+       
         try:
-            id=self.user.get('user')
-            cur.execute("""SELECT * FROM user_table WHERE id=%s""",[id])
-            result=cur.fetchone()
-            if not result:
-                return False
+            id=self.user.get('user_id')
+            party_id=self.user.get('party_id')
+            print(self.office)
             
-            return True
+            cur.execute("""INSERT INTO candidate(office_id,candidate_id,party_id)
+            VALUES (%s,%s,%s);""",(self.office,id,party_id))
+            conn.commit()
+            return{
+                'status':True
+            }
         except:
-            return False
+            message='error occured when inserting into the database please make sure that the party_id,user_id are correct'
+            return {
+                'status':False,
+                'message':message   
+            }
 
         
--- api/v2/views/candidate_view/candidateView.py
+++ api/v2/views/candidate_view/candidateView.py
@@ -5,9 +5,10 @@ candidate_bp=Blueprint('candidate',__name__,url_prefix='/api/v2')
 
 @candidate_bp.route('office/<int:office_id>/register',methods=['POST'])
 def candidate(office_id):
-    user_id=request.get_json()
-    result=candidates(user_id).registerCandidate()
-    if result == False:
-        return jsonify({"error":"no user with that id passed"})
-    candidate={'office':office_id,"user":user_id['user']}
+    data=request.get_json()
+    result=candidates(data,office_id).registerCandidate()
+    if result['status'] == False:
+        return jsonify({"error":result['message']})
+    
+    candidate={'office':office_id,"user":data['user_id']}
     return jsonify({"data":candidate})
