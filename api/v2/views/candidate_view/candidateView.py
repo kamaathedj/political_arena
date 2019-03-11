597f5eefa1eca9d445ae830b70649669c9976751
kamaathedj <kamaudavikiruku@gmail.com>
2019-03-11 09:49:06 +0300

    added token to check that it is the admin who can register user as candidate

--- api/v2/views/candidate_view/candidateView.py
+++ api/v2/views/candidate_view/candidateView.py
@@ -1,10 +1,14 @@
 from flask import Blueprint,request,jsonify
 from api.v2.models.candidate_model.candidateModel import candidates
+from api.v2.views.auth_view.user_view import token_required
 
 candidate_bp=Blueprint('candidate',__name__,url_prefix='/api/v2')
 
 @candidate_bp.route('office/<int:office_id>/register',methods=['POST'])
-def candidate(office_id):
+@token_required
+def candidate(resp,office_id):
+    if resp[9]==False:
+        return jsonify({"message":"you are not the admin"})
     data=request.get_json()
     result=candidates(data,office_id).registerCandidate()
     if result['status'] == False:
